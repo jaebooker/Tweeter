17ee2d8f45ebf23903fedcfb7e568a3987157d61
jaebooker <jaesonbooker@gmail.com>
2018-10-24 20:02:57 -0700

    made slightly-less hacky version

--- rearrange.py
+++ rearrange.py
@@ -9,21 +9,15 @@ quotes = ("Always look on the bright side of life.",
 def random_python_quote():
     rand_index = random.randint(0, len(quotes) - 1)
     return quotes[rand_index]
-
 def random_word_generator():
     with open('./words.txt') as w:
         rwords = w.read().split()
-    rand_index = random.randint(0, len(rwords) - 1)
-    random_word = rwords[rand_index]
-    rand_index = random.randint(0, len(rwords) - 1)
-    random_word2 = rwords[rand_index]
-    rand_index = random.randint(0, len(rwords) - 1)
-    random_word3 = rwords[rand_index]
-    rand_index = random.randint(0, len(rwords) - 1)
-    random_word4 = rwords[rand_index]
-    rand_index = random.randint(0, len(rwords) - 1)
-    random_word5 = rwords[rand_index]
-    print("{random_word} {random_word2} {random_word3} {random_word4} {random_word5}!".format(random_word=random_word, random_word2=random_word2, random_word3=random_word3, random_word4=random_word4, random_word5=random_word5))
+    random_index = []
+    for i in range(0,5):
+        random_number = random.randint(0, len(rwords) - 1)
+        i = rwords[random_number]
+        random_index.append(i)
+    print("{random_word} {random_word2} {random_word3} {random_word4} {random_word5}!".format(random_word=random_index[0], random_word2=random_index[1], random_word3=random_index[2], random_word4=random_index[3], random_word5=random_index[4]))
 
 if __name__ == '__main__':
     quote = random_python_quote()

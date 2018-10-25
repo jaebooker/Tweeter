5fd392a7c88030eb84c7c0e3a33cc892d15f5c01
jaebooker <jaesonbooker@gmail.com>
2018-10-24 17:08:59 -0700

    created random word generator using giant dictionary

--- rearrange.py
+++ rearrange.py
@@ -1,13 +1,23 @@
 import random
 
-quotes = ("It's just a flesh wound.",
-          "He's not the Messiah. He's a very naughty boy!",
+quotes = ("Always look on the bright side of life.",
+          "You just contradicted me.",
+          "No, I didn't!",
+          "Yes you did!",
           "THIS... IS AN EX-PARROT!!")
 
 def random_python_quote():
     rand_index = random.randint(0, len(quotes) - 1)
     return quotes[rand_index]
 
+def random_word_generator():
+    with open('./words.txt') as w:
+        rwords = w.read().split()
+    rand_index = random.randint(0, len(rwords) - 1)
+    random_word = rwords[rand_index]
+    print(random_word)
+
 if __name__ == '__main__':
     quote = random_python_quote()
     print(quote)
+    random_word_generator()

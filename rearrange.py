f5de139efb9911315f514eea16c7878e6350aa6f
jaebooker <jaesonbooker@gmail.com>
2018-10-24 17:15:20 -0700

    made super hacky sentence generator

--- rearrange.py
+++ rearrange.py
@@ -15,7 +15,15 @@ def random_word_generator():
         rwords = w.read().split()
     rand_index = random.randint(0, len(rwords) - 1)
     random_word = rwords[rand_index]
-    print(random_word)
+    rand_index = random.randint(0, len(rwords) - 1)
+    random_word2 = rwords[rand_index]
+    rand_index = random.randint(0, len(rwords) - 1)
+    random_word3 = rwords[rand_index]
+    rand_index = random.randint(0, len(rwords) - 1)
+    random_word4 = rwords[rand_index]
+    rand_index = random.randint(0, len(rwords) - 1)
+    random_word5 = rwords[rand_index]
+    print("{random_word} {random_word2} {random_word3} {random_word4} {random_word5}!".format(random_word=random_word, random_word2=random_word2, random_word3=random_word3, random_word4=random_word4, random_word5=random_word5))
 
 if __name__ == '__main__':
     quote = random_python_quote()

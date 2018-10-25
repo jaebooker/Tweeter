6a61d440e1be589792a5c02d447f7c343013e8dd
jaebooker <jaesonbooker@gmail.com>
2018-10-24 20:31:33 -0700

    made it more speed optimal

--- rearrange.py
+++ rearrange.py
@@ -9,17 +9,20 @@ quotes = ("Always look on the bright side of life.",
 def random_python_quote():
     rand_index = random.randint(0, len(quotes) - 1)
     return quotes[rand_index]
-def random_word_generator():
+def random_word_generator(number_input):
     with open('./words.txt') as w:
         rwords = w.read().split()
-    random_index = []
-    for i in range(0,5):
+    random_index = ""
+    for i in range(0,number_input):
         random_number = random.randint(0, len(rwords) - 1)
-        i = rwords[random_number]
-        random_index.append(i)
-    print("{random_word} {random_word2} {random_word3} {random_word4} {random_word5}!".format(random_word=random_index[0], random_word2=random_index[1], random_word3=random_index[2], random_word4=random_index[3], random_word5=random_index[4]))
+        random_word = rwords[random_number]
+        if i != number_input-1:
+            random_index += str(random_word + " ")
+        else:
+            random_index += str(random_word + "!")
+    print(random_index)
 
 if __name__ == '__main__':
     quote = random_python_quote()
     print(quote)
-    random_word_generator()
+    random_word_generator(5)

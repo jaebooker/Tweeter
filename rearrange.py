8d1db44a984a36fc30ae47b734e9e423d3455a93
jaebooker <jaesonbooker@gmail.com>
2018-10-24 21:29:08 -0700

    added foxsay

--- rearrange.py
+++ rearrange.py
@@ -5,7 +5,23 @@ quotes = ("Always look on the bright side of life.",
           "No, I didn't!",
           "Yes you did!",
           "THIS... IS AN EX-PARROT!!")
-
+fox_word = ""
+#Image below thanks to some guy named Todd Vargo... so thanks, Todd
+fox = ["   /\   /\ ","\
+  //\\_//\\  _","\
+  \_     _/    /   /","\
+   / * * \    /^^^]","\
+   \_\O/_/    [   ]","\
+    /   \_    [   /","\
+    \     \_  /  /","\
+     [ [ /  \/ _/","\
+    _[ [ \  /_/"]
+def what_the_fox_say(fword):
+    fox_word = fword
+    print(fox[0])
+    print(fox[1] + fox_word)
+    for i in range(2,9):
+        print(fox[i])
 def random_python_quote():
     rand_index = random.randint(0, len(quotes) - 1)
     return quotes[rand_index]
@@ -14,17 +30,17 @@ def random_word_generator(number_input):
         rwords = w.read().split()
     random_index = ""
     random_punc = [".","?","!","..."]
-    for i in range(0,number_input):
+    for x in range(0,number_input):
         random_number = random.randint(0, len(rwords) - 1)
         random_word = rwords[random_number]
-        if i != number_input-1:
+        if x != number_input-1:
             random_index += str(random_word + " ")
         else:
-            new_random_number = random.randint(0, 4)
+            new_random_number = random.randint(0, 3)
             random_index += str(random_word + random_punc[new_random_number])
-    print(random_index)
+    return random_index
 
 if __name__ == '__main__':
     quote = random_python_quote()
-    print(quote)
-    random_word_generator(5)
+    what_the_fox_say(quote)
+    what_the_fox_say(random_word_generator(5))

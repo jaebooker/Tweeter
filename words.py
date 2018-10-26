6a4006421269cae32a68976c2f3696941d9a8dca
jaebooker <jaesonbooker@gmail.com>
2018-10-25 17:08:24 -0700

    added timer

--- words.py
+++ words.py
@@ -1,11 +1,15 @@
 import random
+import time
 from monty import *
 from WhatTheFoxSay import *
 from fisher import *
 from randomdict import *
+
+stime = time.time()
 
 if __name__ == '__main__':
     quote = random_python_quote()
     what_the_fox_say(quote)
     what_the_fox_say(random_word_generator(5))
     print(fisher_python(["words", "notwords", "wordy", "worthy"]))
+    print(time.time()-stime)

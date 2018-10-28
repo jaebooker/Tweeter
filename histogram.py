493c39e75b56c4758e4b1eb25a473232d3667a22
jaebooker <jaesonbooker@gmail.com>
2018-10-27 19:53:22 -0700

    built hacky histogram with fish

---
+++ histogram.py
@@ -0,0 +1,23 @@
+fishy_python = "one fish, two fish, red fish, blue fish, you dish"
+def histogram(words):
+    words_list = words.split()
+    word_dictionary = {}
+    counter = len(words_list)-1
+    i = 0
+    while i < counter:
+        word_dictionary[words_list[i]] = 1
+        n = i+1
+        while n < counter:
+            if words_list[i] == words_list[n]:
+                word_dictionary[words_list[i]] += 1
+                words_list.pop(n)
+                counter -= 1
+            n += 1
+        i += 1
+    return word_dictionary
+
+print(histogram(fishy_python))
+def unique_words(histogram):
+    pass
+def frequency(word, histogram):
+    pass

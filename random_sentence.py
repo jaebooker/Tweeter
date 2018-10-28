e719b2e949ffbc3da8332a515e92cf41597db837
jaebooker <jaesonbooker@gmail.com>
2018-10-27 22:51:49 -0700

    made random words based on frequency used and compare it with non-freqeuncy based ones

--- random_sentence.py
+++ random_sentence.py
@@ -1,6 +1,14 @@
+import random
 from histogram import *
 from fisher import *
 def random_word_from_histogram(histogram):
     histo_array = create_sorted_array(histogram)
     fisher_python(histo_array)
     return histo_array[0][1]
+def stochastic_random_word(histogram):
+    new_histo_array = create_sorted_array(histogram)
+    fisher_python(new_histo_array)
+    for i in range(0,len(new_histo_array)):
+        random_number = random.randrange(0,100)
+        if (new_histo_array[i][0]/100) > (random_number/100):
+            return new_histo_array[i][1]

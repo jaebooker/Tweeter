2eb0d6a326e83bb3419b1244430358929fe9e780
jaebooker <jaesonbooker@gmail.com>
2018-10-29 02:26:15 -0700

    made some imrpovements?

--- random_spinner.py
+++ random_spinner.py
@@ -4,17 +4,12 @@ import time
 def random_spinner(min_range, max_range):
     time_add = (min_range + max_range) / 100
     start_point = min_range
-    end_point = max_range
     start_time = time.time()
     while (time.time()-start_time) < 0.00011:
         if start_point < max_range:
             start_point += time_add
         else:
             start_point = min_range
-    start_time = time.time()
-    while (time.time()-start_time) < 0.00011:
-        if end_point > min_range:
-            end_point -= time_add
-        else:
-            end_point = max_range
-    return (start_point+end_point)/2
+    return start_point
+
+print(random_spinner(2,200))

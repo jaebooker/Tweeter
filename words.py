32f3341190dc64e83652f9fd825e66709abf308f
jaebooker <jaesonbooker@gmail.com>
2018-10-30 15:54:28 -0700

    brought the fox back

--- words.py
+++ words.py
@@ -8,9 +8,9 @@ from randomdict import *
 stime = time.time()
 
 if __name__ == '__main__':
-    # quote = random_python_quote()
-    # what_the_fox_say(quote)
-    # what_the_fox_say(random_word_generator(5))
+    quote = random_python_quote()
+    what_the_fox_say(quote)
+    what_the_fox_say(random_word_generator(5))
     # print(fisher_python(["words", "notwords", "wordy", "worthy"]))
-    # print(time.time()-stime)
-    print(random_word_generator(5))
+    print(time.time()-stime)
+    # print(random_word_generator(5))

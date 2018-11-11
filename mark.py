2b361f8043b55c0b7d5321b210b1a7319bb07e61
jaebooker <jaesonbooker@gmail.com>
2018-11-11 01:13:22 -0800

    made basic markdown chain

---
+++ mark.py
@@ -0,0 +1,31 @@
+import random
+def markdown(word_list, sentence_length):
+    chain = []
+    random_number = random.randrange(0,len(word_list))
+    random_next_word = word_list[random_number]
+    chain.append(random_next_word)
+    for i in range(0, sentence_length):
+        next_word = markdown_unchained(word_list, random_next_word)
+        if next_word:
+            chain.append(next_word)
+            random_next_word = next_word
+        else:
+            return chain
+    return chain
+
+def markdown_unchained(word_list, random_next_word):
+    unchained = []
+    for i in range(0,len(word_list)):
+        if random_next_word == word_list[i]:
+            if i == len(word_list)-1:
+                break
+            unchained.append(word_list[i+1])
+    if unchained:
+        random_index = random.randrange(0,len(unchained))
+        next_word = unchained[random_index]
+        return next_word
+    else:
+        return None
+
+wlist = ['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish', 'pasta']
+print(markdown(wlist, 10))

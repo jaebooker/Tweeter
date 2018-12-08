745fa718fc29d4d2823e286341f7592a4e6d4605
jaebooker <jaesonbooker@gmail.com>
2018-12-08 00:54:57 -0800

    stirner time

--- stirner.py
+++ stirner.py
@@ -1,4 +1,5 @@
 import re
+import random
 from mark import *
 from markdown_mark2 import *
 #from flask import Flask
@@ -9,11 +10,15 @@ from markdown_mark2 import *
 # @app.route("/")
 with open('./stirner.txt') as w:
     stirner_text = w.read()
+with open('./trends.txt') as w:
+    trend_text = w.read()
+trend_list = trend_text.split()
+trend_word = trend_list[random.randrange(0,len(trend_list))]
 word_list = re.split("\W*[^\'\w+\']", stirner_text)
 #holmes_sentence = markdown(word_list, 20)
-stirner_sentence = markdown2(word_list, 50)
+stirner_sentence = markdown2(word_list, 20)
 stirner_string = ""
 for i in stirner_sentence:
     stirner_string += " "
     stirner_string += i
-print(stirner_string)
+print(trend_word + stirner_string)
